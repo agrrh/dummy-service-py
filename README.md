ad5889d5d660001b2bf646f082d6e9b85a9eea64
Kirill K <kovalev.kirill.a@gmail.com>
2017-12-21 22:04:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a dummy python service which listens on `0.0.0.0:9005` and answers with following JSON structure:
+
+```
+{
+    "hostname": "your-hostname-here", 
+    "uuid": "random-uuid-here"
+}
+```
---
+++ daemon.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from socket import gethostname
+from uuid import uuid4 as uuid
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({
+        'hostname': gethostname(),
+        'uuid': uuid()
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9005)
